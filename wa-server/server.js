24e5b0beb6e3641b373e9593190d7071f144b24e
Nikita <ifyoucan1337@yandex.ru>
2023-02-16 23:10:51 +0300

    create server

---
+++ wa-server/server.js
@@ -0,0 +1,29 @@
+import 'colors';
+import * as dotenv from 'dotenv';
+import express from 'express';
+import morgan from 'morgan';
+
+import authRoutes from './app/auth/auth.routes.js';
+
+const app = express();
+dotenv.config();
+
+async function main() {
+	if (process.env.NODE_ENV === 'development') {
+		app.use(morgan('dev'));
+	}
+
+	app.use(express.json());
+	app.use('/api/auth', authRoutes);
+
+	const PORT = process.env.PORT || 5000;
+
+	app.listen(
+		PORT,
+		console.log(
+			`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`.green
+		)
+	);
+}
+
+main();
