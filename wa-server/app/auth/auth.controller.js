30f162262447faf57816b84d3f3c73f5b72675bf
Nikita <ifyoucan1337@yandex.ru>
2023-02-17 14:27:25 +0300

    add registerUser

--- wa-server/app/auth/auth.controller.js
+++ wa-server/app/auth/auth.controller.js
@@ -1,13 +1,63 @@
+import { faker } from '@faker-js/faker';
+import { hash, verify } from 'argon2';
 import asyncHandler from 'express-async-handler';
 
 import { prisma } from '../prisma.js';
+
+import { generateToken } from './generate-token.js';
 
 // @desc Auth user
 // @route POST /api/auth/login
 // @access Public
 
 export const authUser = asyncHandler(async (req, res) => {
-	const user = await prisma.user.findMany();
+	const { email, password } = req.body;
 
-	res.json(user);
+	const user = await prisma.user.findUnique({
+		where: {
+			email
+		}
+	});
+
+	const isPasswordMatch = await verify(user.password, password);
+
+	if (user && isPasswordMatch) {
+		const token = generateToken(user.id);
+
+		res.json({ user, token });
+	} else {
+		res.status(401);
+		throw new Error(`Email or password is not correct`);
+	}
+});
+
+// @desc Auth user
+// @route POST /api/auth/register
+// @access Public
+
+export const registerUser = asyncHandler(async (req, res) => {
+	const { email, password } = req.body;
+
+	const isHaveUser = await prisma.user.findUnique({
+		where: {
+			email
+		}
+	});
+
+	if (isHaveUser) {
+		res.status(400);
+		throw new Error('User is already exists');
+	}
+
+	const user = await prisma.user.create({
+		data: {
+			email,
+			password: await hash(password),
+			name: faker.name.fullName()
+		}
+	});
+
+	const token = generateToken(user.id);
+
+	res.json({ user, token });
 });
