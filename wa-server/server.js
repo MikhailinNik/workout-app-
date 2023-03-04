32bc6255e2b0be076d82e8ed5c091312f1660a01
Nikita <ifyoucan1337@yandex.ru>
2023-03-04 12:27:56 +0300

    add cors policy

--- wa-server/server.js
+++ wa-server/server.js
@@ -1,4 +1,5 @@
 import 'colors';
+import cors from 'cors';
 import * as dotenv from 'dotenv';
 import express from 'express';
 import morgan from 'morgan';
@@ -20,6 +21,7 @@ async function main() {
 		app.use(morgan('dev'));
 	}
 
+	app.use(cors());
 	app.use(express.json());
 
 	const __dirname = path.resolve();
